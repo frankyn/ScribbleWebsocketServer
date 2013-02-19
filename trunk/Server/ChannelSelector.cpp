095876e2bc5e0d7224d5104c846b88317e311469
Frank Natividad <frankyn@nmsu.edu>
2013-02-19 14:09:14 -0700

    Brute force to keeping thread alive if it dies.

--- trunk/Server/ChannelSelector.cpp
+++ trunk/Server/ChannelSelector.cpp
@@ -20,59 +20,62 @@ void ChannelSelector::Setup ( ) {
 }
 
 void ChannelSelector::Execute ( void * arg ) {
-    try{
-		char buffer[1024] = {0};
-		int bytes = 0, desc = 0;
-		WSAttributes attributes;
-		Connection * newConnection = NULL;
-		WSProtocol * protocol = NULL;
-		Log ( "Channel Selector: started" );
-		while ( status ) { 
-			newConnection = NULL;
-			protocol = NULL;	
-			if ( connectionsWaiting->wait ( ) ) {
-				continue;
-			}
-			connectionsWaiting->lock ( );
-			desc = connectionsWaiting->get ( );
-			connectionsWaiting->unlock ( );	
-			Log ( "Channel Selector: new connection" );	
-			protocol = new RFC_6455();
-			newConnection = new Connection ( desc, protocol );
-			Log ( "Channel Selector: waiting for handshake" );
-			bytes = newConnection->recv ( buffer, sizeof(buffer)/sizeof(char) );
-			Log ( "Channel Selector: handshake checking" );
-			if ( bytes ) {				
-				Log ( "Channel Selector: handshake received" );
-				if ( newConnection->handshake ( buffer, &attributes ) == 0 ){
-					/*		
-					std::cout<<"[Version] " << attributes.version << std::endl;
-					std::cout<<"[Channel] " << attributes.channel << std::endl;
-					std::cout<<"[Response] " << std::endl << attributes.response << std::endl;
-					*/
-					//std::cout<<"[Channel] " << attributes.channel << std::endl;
-					newConnection->send ( attributes.response.c_str(), attributes.response.length() );
-					if ( channels->find( attributes.channel ) != channels->end() ) {
-						//std::cout<< "Connection sent to: " << attributes.channel<<std::endl;
-						Log ( "Channel Selector: Connection sent to: " + attributes.channel );
-						(*channels) [ attributes.channel ]->addConnection ( newConnection );
-						newConnection = NULL;
-						protocol = NULL;
-					} else {
-						Log ( "Channel Selector: Channel Not found" );
-						newConnection->close ( );
-						//delete newConnection;//Protocol is freed within Connection()
-						//newConnection = NULL;
-						//protocol = NULL;
-					}
+	while ( status ) {
+	    try{
+			char buffer[1024] = {0};
+			int bytes = 0, desc = 0;
+			WSAttributes attributes;
+			Connection * newConnection = NULL;
+			WSProtocol * protocol = NULL;
+			Log ( "Channel Selector: started" );
+			while ( status ) { 
+				newConnection = NULL;
+				protocol = NULL;	
+				if ( connectionsWaiting->wait ( ) ) {
 					continue;
 				}
-			}
-			//delete newConnection;//Protocol is freed within Connection()
-			//newConnection = NULL;
-			//protocol = NULL;
-   		}
-    } catch(std::exception& e){
-		Log ( "Channel Selector: failed. --" + std::string(e.what ( )) );
-    }
+				connectionsWaiting->lock ( );
+				desc = connectionsWaiting->get ( );
+				connectionsWaiting->unlock ( );	
+				Log ( "Channel Selector: new connection" );	
+				protocol = new RFC_6455();
+				newConnection = new Connection ( desc, protocol );
+				Log ( "Channel Selector: waiting for handshake" );
+				bytes = newConnection->recv ( buffer, sizeof(buffer)/sizeof(char) );
+				Log ( "Channel Selector: handshake checking" );
+				if ( bytes ) {				
+					Log ( "Channel Selector: handshake received" );
+					if ( newConnection->handshake ( buffer, &attributes ) == 0 ){
+						/*		
+						std::cout<<"[Version] " << attributes.version << std::endl;
+						std::cout<<"[Channel] " << attributes.channel << std::endl;
+						std::cout<<"[Response] " << std::endl << attributes.response << std::endl;
+						*/
+						//std::cout<<"[Channel] " << attributes.channel << std::endl;
+						newConnection->send ( attributes.response.c_str(), attributes.response.length() );
+						if ( channels->find( attributes.channel ) != channels->end() ) {
+							//std::cout<< "Connection sent to: " << attributes.channel<<std::endl;
+							Log ( "Channel Selector: Connection sent to: " + attributes.channel );
+							(*channels) [ attributes.channel ]->addConnection ( newConnection );
+							newConnection = NULL;
+							protocol = NULL;
+						} else {
+							Log ( "Channel Selector: Channel Not found" );
+							newConnection->close ( );
+							//delete newConnection;//Protocol is freed within Connection()
+							//newConnection = NULL;
+							//protocol = NULL;
+						}
+						continue;
+					}
+				}
+				//delete newConnection;//Protocol is freed within Connection()
+				//newConnection = NULL;
+				//protocol = NULL;
+	   		}
+	   		break;
+	    } catch(std::exception& e){
+			Log ( "Channel Selector: failed. --" + std::string(e.what ( )) );
+	    }
+	}
 }
