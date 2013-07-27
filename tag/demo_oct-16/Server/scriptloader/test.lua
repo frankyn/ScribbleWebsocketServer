aee849aeceb54f2cd5eaa9531ef0936167817f6d
Frank Natividad <contact@franknatividad.com>
2013-07-27 00:27:53 -0700

    Moved and organized the repo to fit a more git oriented version control. It was originally setup for subversion.

--- tag/demo_oct-16/Server/scriptloader/test.lua
+++
@@ -1,4 +0,0 @@
-function run ( input )
-	output = "Amanda is cool"
-	return output
-end
--- tag/demo_oct-2/Server/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "./common/Logger.h"
-#include "./ListenerClass.h"
-
-int main(){
-	ListenerClass LS = ListenerClass(1035,10);
-	LS.Wait();
-	
-	return 0;
-}
