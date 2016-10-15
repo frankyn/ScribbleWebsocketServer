6e36a7e8773fc25a107d1ba55b7ebf4cd09a6d10
Frank Natividad <contact@franknatividad>
2016-10-14 23:16:05 -0700

    Removing old html samples

--- examples/rpg_tutorial/enchant/dev/src/Game.js
+++
@@ -1,5 +0,0 @@
-/**
- * enchant.Core is moved to enchant.Core from v0.6
- * @type {*}
- */
-enchant.Game = enchant.Core;
--- examples/rpg_tutorial/enchant/dev/src/Sound.js
+++
@@ -1,3 +0,0 @@
-/* jshint newcap: false */
-
-enchant.Sound = window.AudioContext && enchant.ENV.USE_WEBAUDIO ? enchant.WebAudioSound : enchant.DOMSound;
