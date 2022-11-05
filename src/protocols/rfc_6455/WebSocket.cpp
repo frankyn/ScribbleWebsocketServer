3619052f78527809ce6096936b86d9706d355653
frankyn <contact@franknatividad.com>
2022-11-04 20:38:03 -0700

    feat: refactor WebSocket class

--- src/protocols/rfc_6455/WebSocket.cpp
+++ src/protocols/rfc_6455/WebSocket.cpp
@@ -1,53 +1,79 @@
 #include "WebSocket.h"
 
-using namespace std;
-using namespace boost::uuids::detail;
-using namespace boost::archive::iterators;
+namespace scribble {
+namespace websocket {
+using boost::archive::iterators::base64_from_binary;
+using boost::archive::iterators::transform_width;
+using boost::json::string_view;
+using boost::uuids::detail::sha1;
+using std::cout;
+using std::invalid_argument;
+using std::ostream_iterator;
+using std::shared_ptr;
+using std::string;
+using std::stringstream;
 
-WebSocket::WebSocket() {}
+WebSocket::WebSocket(uint64_t t_version, string t_channel, string t_guid)
+    : m_version(t_version), m_channel(t_channel), m_guid(t_guid) {
+  if (m_version != 13) {
+    throw invalid_argument("WebSocket version 13 is the only supported");
+  }
+}
+
+WebSocket::WebSocket(const string &t_data) {
+  ParseVersion(t_data);
+  ParseChannel(t_data);
+  ParseGuid(t_data);
+  if (m_version != 13) {
+    throw invalid_argument("WebSocket version 13 is the only supported");
+  }
+}
 
 WebSocket::~WebSocket() {}
 
-int WebSocket::handshake(const string &input, WSAttributes &response) {
-  /*Grab Version*/
-  if (input.compare("") == 0)
-    return -1;
-  string versionLookupStart = "Sec-WebSocket-Version: ";
-  string versionLookupEnd = "\r\n";
-  int verStart = input.find(versionLookupStart);
-  int verEnd = input.find(versionLookupEnd, verStart);
-  response.version =
-      input.substr(verStart + versionLookupStart.length(),
-                   verEnd - (verStart + versionLookupStart.length()));
-  if (response.version.compare("13") != 0)
-    return -1;
+const uint64_t WebSocket::GetVersion() { return m_version; }
+
+const std::string WebSocket::GetChannel() { return m_channel; }
+
+void WebSocket::ParseChannel(const string &t_data) {
   /*Grab Channel Name*/
   string channelLookupStart = "GET /";
   string channelLookupEnd = " HTTP/1.1";
-  int channelStart = input.find(channelLookupStart);
-  int channelEnd = input.find(channelLookupEnd, channelStart);
-  response.channel =
-      input.substr(channelStart + channelLookupStart.length(),
-                   channelEnd - (channelStart + channelLookupStart.length()));
-  /*Create Server Response*/
-  stringstream os;
-  string GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-  os << "HTTP/1.1 101 Switching Protocols\r\n";
-  os << "Upgrade: websocket\r\n";
-  os << "Connection: Upgrade\r\n";
-  os << "Sec-WebSocket-Accept: ";
+  int channelStart = t_data.find(channelLookupStart);
+  int channelEnd = t_data.find(channelLookupEnd, channelStart);
+  m_channel =
+      t_data.substr(channelStart + channelLookupStart.length(),
+                    channelEnd - (channelStart + channelLookupStart.length()));
+}
+
+void WebSocket::ParseVersion(const string &t_data) {
+  const string versionLookupStart = "Sec-WebSocket-Version: ";
+  const string versionLookupEnd = "\r\n";
+  const int verStart = t_data.find(versionLookupStart);
+  const int verEnd = t_data.find(versionLookupEnd, verStart);
+  m_version =
+      ::atol(t_data
+                 .substr(verStart + versionLookupStart.length(),
+                         verEnd - (verStart + versionLookupStart.length()))
+                 .c_str());
+}
+
+void WebSocket::ParseGuid(const string &t_data) {
+  const string GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
   string keyLookupStart = "Sec-WebSocket-Key: ";
   string keyLookupEnd = "\r\n";
-  int keyStart = input.find(keyLookupStart);
-  int keyEnd = input.find(keyLookupEnd, keyStart);
-  string inputKey =
-      input.substr(keyStart + keyLookupStart.length(),
-                   keyEnd - (keyStart + keyLookupStart.length())) +
-      GUID;
+  int keyStart = t_data.find(keyLookupStart);
+  int keyEnd = t_data.find(keyLookupEnd, keyStart);
+  m_guid = t_data.substr(keyStart + keyLookupStart.length(),
+                         keyEnd - (keyStart + keyLookupStart.length())) +
+           GUID;
+}
+
+void WebSocket::AddDigest(stringstream &os) {
   unsigned int digest[5];
   unsigned char longDigest[20];
   sha1 sha1;
-  sha1.process_bytes(inputKey.c_str(), inputKey.length());
+  sha1.process_bytes(m_guid.c_str(), m_guid.length());
   sha1.get_digest(digest);
   for (int i = 0, j = 0; i < 5; i++) {
     longDigest[j++] = (digest[i] & 0xFF000000) >> 24;
@@ -57,234 +83,66 @@ int WebSocket::handshake(const string &input, WSAttributes &response) {
   }
   typedef base64_from_binary<transform_width<const char *, 6, 8>> base64_text;
   copy(base64_text(longDigest), base64_text(longDigest + 20),
-       std::ostream_iterator<char>(os));
-  os << '=';
-  os << "\r\n\r\n";
-  response.response = os.str();
-
-  return 0;
+       ostream_iterator<char>(os));
 }
 
-/*
-        Check if the packet has the MASK bit turned on.
-*/
-int WebSocket::hasMask(const string &data) {
-  if (data.empty())
-    return 0;
-  return (data[1] & 0x80);
+const std::string WebSocket::GetHandshake() {
+  stringstream os;
+  os << "HTTP/1.1 101 Switching Protocols\r\n";
+  os << "Upgrade: websocket\r\n";
+  os << "Connection: Upgrade\r\n";
+  os << "Sec-WebSocket-Accept: ";
+  AddDigest(os);
+  os << "=\r\n\r\n";
+  return os.str();
 }
 
-/*
-        Check if the packet is fragmented state
-*/
-
-int WebSocket::packetFragmented(const string &data) {
-  if (data.empty())
+const size_t WebSocket::Decode(const string &t_data, string &t_decoded_data,
+                               const size_t t_bytes_to_read) {
+  // TODO: Handle multi frame messages
+  if (t_data.empty() || t_data.size() < 2) {
     return 0;
-  return !(data[0] & 0x80);
-}
-
-/*
-        Check if the packet is complete.
-*/
-
-int WebSocket::packetComplete(const string &input) {
-  if (input.empty())
-    return 0;
-  std::string tmp = input;
-  int completed = 0;
-  // Using a loop instead of using recursion.
-  unsigned long len = packetRealLength(tmp);
-  if (len != 0 && len <= input.size()) {
-    completed = 1;
   }
-
-  return completed;
-}
-
-/*
-        Get full size of packet even if it's fragged.
-*/
-
-unsigned long WebSocket::packetRealLength(const string &data) {
-  if (data.empty())
-    return 0;
-  std::string tmp = data;
-  int foundEndPacket = 0;
-  unsigned long sizeOfPacket = 0;
-  // Using a loop instead of using recursion.
-  while (!tmp.empty() && !foundEndPacket) {
-    if (packetFragmented(tmp)) {
-      // Packet is a partial let's check to make sure all the packets are here
-      // before we allow decoding.
-      WSPacketLength pcktLen;
-      packetLength(tmp, pcktLen);
-      sizeOfPacket += pcktLen.packetLen;
-      if (sizeOfPacket > data.size()) {
-        // Incomplete packet
-        return 0;
-      }
-      tmp = tmp.substr(pcktLen.packetLen, tmp.size());
-
-    } else {
-      WSPacketLength pcktLen;
-      packetLength(tmp, pcktLen);
-      sizeOfPacket += pcktLen.packetLen;
-      if (sizeOfPacket > data.size()) {
-        // Incomplete packet
-        return 0;
-      }
-      foundEndPacket = 1;
-    }
-  }
-  return sizeOfPacket;
-}
-
-/*
-        Check packet length:
-                There are 3 different cases for large packet sizes.
-*/
-void WebSocket::packetLength(const string &data, WSPacketLength &pcktLen) {
-  pcktLen.length = 0;
-  pcktLen.payloadOffset = 0;
-  pcktLen.packetLen = 0;
-
-  if (data.empty()) {
-    return;
-  }
-  /*
-        0                   1                   2                   3
-0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-+-+-+-+-+-------+-+-------------+-------------------------------+
-|F|R|R|R| opcode|M| Payload len |    Extended payload length    |
-|I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
-|N|V|V|V|       |S|             |   (if payload len==126/127)   |
-| |1|2|3|       |K|             |                               |
-+-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
-|     Extended payload length continued, if payload len == 127  |
-+ - - - - - - - - - - - - - - - +-------------------------------+
-|                               |Masking-key, if MASK set to 1  |
-+-------------------------------+-------------------------------+
-| Masking-key (continued)       |          Payload Data         |
-+-------------------------------- - - - - - - - - - - - - - - - +
-:                     Payload Data continued ...                :
-+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
-|                     Payload Data continued ...                |
-+---------------------------------------------------------------+
-  */
-  const unsigned char *inputBytes = (unsigned char *)data.c_str();
-  pcktLen.length = inputBytes[1] & 0x7F;
-
-  if (pcktLen.length == 126) {
-    // 126 - 2 bytes
-    // std::cout<<"Ver: 126 Size"<<std::endl;
-    pcktLen.length = inputBytes[3] | inputBytes[2] << 8;
-    pcktLen.payloadOffset = 4;
-    pcktLen.packetLen =
-        pcktLen.payloadOffset + (hasMask(data) ? 4 : 0) + pcktLen.length;
-  } else if (pcktLen.length == 127) {
-    // 127 - 8 bytes
-    // NOT TESTED || Need a test packet.
-    // std::cout<<"Ver: 127 Size"<<std::endl;
-    pcktLen.length = inputBytes[2] << 24 | inputBytes[3] << 16 |
-                     inputBytes[4] << 8 | inputBytes[5];
-    pcktLen.payloadOffset = 6;
-    pcktLen.packetLen =
-        pcktLen.payloadOffset + (hasMask(data) ? 4 : 0) + pcktLen.length;
+  bool complete = t_data[0] & 0x80;
+  size_t payload_length = 0;
+  size_t payload_offset = 2;
+  if (t_data[1] == 0x7E) {
+    payload_length = t_data[2] << 8 | t_data[3];
+    payload_offset += 2;
+  } else if (t_data[1] == 0x7F) {
+    payload_length =
+        t_data[2] << 24 | t_data[3] << 16 | t_data[4] << 8 | t_data[5];
+    payload_offset += 4;
   } else {
-    // std::cout<<"Ver: 125 Size"<<std::endl;
-    pcktLen.payloadOffset = 2;
-    pcktLen.packetLen =
-        pcktLen.payloadOffset + (hasMask(data) ? 4 : 0) + pcktLen.length;
+    payload_length = t_data[1] & 0x7D;
   }
+  // TODO: add mask length to payload_offset
+  t_decoded_data = t_data.substr(payload_offset, payload_length);
+  return payload_offset + payload_length;
 }
 
-string WebSocket::decode(const string &data) {
-  string decodedInput = "";
-  /*
-
-     A single-frame unmasked text message
-
-   0x81 0x05 0x48 0x65 0x6c 0x6c 0x6f (contains "Hello")
-
-  A single-frame masked text message
-
-   0x81 0x85 0x37 0xfa 0x21 0x3d 0x7f 0x9f 0x4d 0x51 0x58
-   (contains "Hello")
-
-  */
-  // If packet isn't complete fail decoding.
-  if (!packetComplete(data))
-    return decodedInput;
-  std::string unknownPacket = data;
-
-  do {
-    // std::cout << "LOOP" << std::endl;
-    const char *inputBytes = unknownPacket.c_str();
-    WSPacketLength pcktLen;
-    packetLength(unknownPacket, pcktLen);
-
-    // std::cout << "STARTING DECODING" << std::endl;
-    // std::cout << "INPUT LENGTH: " << input.size() << std::endl;
-    // std::cout << "LENGTH: " << pcktLen.packetLen << std::endl;
-    // std::cout << "LENGTH: " << input.size()  << std::endl;
-    if (hasMask(unknownPacket)) {
-      // std::cout<<"MASKED"<<std::endl;
-      // Masked Input
-      unsigned long byteCounter = 0;
-      unsigned long maskCounter = 0;
-      unsigned long maskOffset = pcktLen.payloadOffset;
-      unsigned long offset = 4 + pcktLen.payloadOffset;
-      // printf ( "---------%X\n", inputBytes[offset] );
-      while (byteCounter < pcktLen.length) {
-        // std::cout << (byteCounter);
-        // printf ( "--0x%X--" , inputBytes[byteCounter+offset] );
-        // std::cout << " " << input.at ( byteCounter ) << " ";
-        // std::cout << " " << (unsigned long)(inputBytes[byteCounter+offset] ^
-        // inputBytes[(maskCounter)+maskOffset]) << std::endl;
-        decodedInput += inputBytes[byteCounter + offset] ^
-                        inputBytes[((maskCounter++) % 4) + maskOffset];
-        byteCounter++;
-      }
-    } else {
-      // Not Masked
-      // std::cout<<"NOT MASKED"<<std::endl;
-      unsigned long byteCounter = 0;
-      int offset = pcktLen.payloadOffset;
-      while (byteCounter < pcktLen.length) {
-        decodedInput += inputBytes[byteCounter + offset];
-        byteCounter++;
-      }
-    }
-    // std::cout << "ENDING DECODING" << std::endl;
-    // std::cout << "Length of unknownPacket: " << unknownPacket.size() <<
-    // std::endl; std::cout << "REMOVING LENGTH: " << pcktLen.packetLen <<
-    // std::endl;
-    unknownPacket = unknownPacket.substr(pcktLen.packetLen);
-    // std::cout << "AFTER SUBSTR" << std::endl;
-  } while (!unknownPacket.empty());
-  return decodedInput;
-}
-
-std::string WebSocket::encode(const string &data) {
-  std::string encodedInput;
-
-  int len = data.length();
-  // std::cout<<"len: "<< len << std::endl;
-
-  encodedInput += "\x81";
-
-  if (len <= 125) {
-    encodedInput += (char)(len);
-  } else if (len <= 65535) {
-    encodedInput += (char)(len >> 8);
-    encodedInput += (char)(len & 0xff);
+const size_t WebSocket::Encode(const string &t_data, string &t_encoded_data,
+                               const size_t t_bytes_to_read) {
+  // TODO: add support for t_bytes_to_read
+  stringstream encoded_output;
+  encoded_output << (uint8_t)0x81;
+  const size_t data_len = t_data.length();
+  if (data_len <= 0x7D) {
+    encoded_output << (uint8_t)(data_len);
+  } else if (data_len <= 0xFFFF) {
+    encoded_output << (uint8_t)0x7E;
+    encoded_output << (uint8_t)((data_len >> 8) & 0xFF);
+    encoded_output << (uint8_t)((data_len & 0xFF));
   } else {
-    // NEEDS TO BE EXTENDED for largest possibility
-    encodedInput += "??????";
+    encoded_output << (uint8_t)0x7F;
+    encoded_output << (uint8_t)((data_len >> 24) & 0xFE);
+    encoded_output << (uint8_t)((data_len >> 16) & 0xFF);
+    encoded_output << (uint8_t)((data_len >> 8) & 0xFF);
+    encoded_output << (uint8_t)((data_len & 0xFF));
   }
-
-  encodedInput += data;
-
-  return encodedInput;
+  encoded_output << t_data;
+  t_encoded_data = encoded_output.str();
+  return t_data.length();
 }
+} // namespace websocket
+} // namespace scribble
--- src/protocols/rfc_6455/WebSocket.h
+++ src/protocols/rfc_6455/WebSocket.h
@@ -1,53 +1,58 @@
+#ifndef WEBSOCKET_HEADER
+#define WEBSOCKET_HEADER
 #include <boost/archive/iterators/base64_from_binary.hpp>
 #include <boost/archive/iterators/insert_linebreaks.hpp>
 #include <boost/archive/iterators/ostream_iterator.hpp>
 #include <boost/archive/iterators/transform_width.hpp>
+#include <boost/json/string_view.hpp>
 #include <boost/uuid/detail/sha1.hpp>
 #include <cstdio>
 #include <cstring>
 #include <fcntl.h>
+#include <iostream>
 #include <sstream>
+#include <stdexcept>
+#include <vector>
 
-#ifndef WEBSOCKET_HEADER
-#define WEBSOCKET_HEADER
-
-/*
-    WEBSOCKET - based on http://tools.ietf.org/html/rfc6455
-*/
-
-struct WSAttributes {
-  std::string version;
-  std::string channel;
-  std::string response;
-};
-
-struct WSPacketLength {
-  unsigned long packetLen;
-  unsigned long length;
-  unsigned payloadOffset;
-};
-
+namespace scribble {
+namespace websocket {
+// WebSocket - based on http://tools.ietf.org/html/rfc6455
 class WebSocket {
 public:
-  WebSocket();
-
+  WebSocket(uint64_t t_version, std::string t_channel, std::string t_guid);
+  WebSocket(const std::string &t_data);
   ~WebSocket();
+  const std::string GetHandshake();
+  /**
+   * @brief Decode data from a frame.
+   * @param t_data Data to decode
+   * @param t_decoded_data Buffer to write decoded data into
+   * @param t_bytes_to_read Number of bytes to read from t_data
+   * @return Number of bytes read from t_data
+   */
+  const size_t Decode(const std::string &t_data, std::string &t_decoded_data,
+                      const size_t t_bytes_to_read);
+  /**
+   * @brief Encode data into a frame of specified size.
+   * @param t_data Data to encode
+   * @param t_encoded_data Buffer to write encoded data into
+   * @param t_bytes_to_read Number of bytes to read from t_data
+   * @return Number of bytes read from t_data
+   */
+  const size_t Encode(const std::string &t_data, std::string &t_encoded_data,
+                      const size_t t_bytes_to_read);
+  const uint64_t GetVersion();
+  const std::string GetChannel();
 
-  void packetLength(const std::string &data, WSPacketLength &ws_packet_length);
-
-  unsigned long packetRealLength(const std::string &data);
-
-  int packetComplete(const std::string &data);
-
-  int packetFragmented(const std::string &data);
-
-  int handshake(const std::string &data, WSAttributes &response);
-
-  int hasMask(const std::string &data);
-
-  std::string decode(const std::string &data);
-
-  std::string encode(const std::string &data);
+private:
+  void ParseVersion(const std::string &t_data);
+  void ParseChannel(const std::string &t_data);
+  void ParseGuid(const std::string &t_data);
+  void AddDigest(std::stringstream &os);
+  uint64_t m_version;
+  std::string m_channel;
+  std::string m_guid;
 };
-
+} // namespace websocket
+} // namespace scribble
 #endif
